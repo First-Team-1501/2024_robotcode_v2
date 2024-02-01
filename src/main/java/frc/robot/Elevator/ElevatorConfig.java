f4dc05f0086426a79ff7e84c4c7e3d9ed35b4e19
Agent33OfChrist <artandbirds@gmail.com>
2024-02-01 17:04:10 -0500

    Elevator & Deck Completed; shooter and stabilizer left

--- src/main/java/frc/robot/Elevator/ElevatorConfig.java
+++ src/main/java/frc/robot/Elevator/ElevatorConfig.java
@@ -1,5 +1,6 @@
 package frc.robot.Elevator;
 
+import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
@@ -36,7 +37,7 @@ public class ElevatorConfig {
     public static int followerID = 0;
 
     //Inverted
-    public static int kInverted = 0;
+    public static boolean kInverted = false;
 
     //Current Limits
     public static int smartCurrentStallLimit = 20;  //40 for big neo, 20 for small neo
@@ -47,13 +48,21 @@ public class ElevatorConfig {
     public static double velocityConversionFactor = 1;
 
     //Soft Limits
-    public static double softLimitFwd = 0;
-    public static double softLimitRev = 0;
+    public static float softLimitFwd = 0;
+    public static float softLimitRev = 0;
+
+        //Soft Limits Enabled
+    public static boolean softLimitFwdEnabled = true;
+    public static boolean softLimitRevEnabled = true;    
 
     //Analog for Hollow Bore
     public static double analogPositionConversion = 1;
     public static double analogVelocityConversion = 1;
     public static int analogSensorMore = 0; // 0 = absolute; 1 = relative
     public static int analogInverted = 0;
+
+    //Control type to position
+    public static ControlType controlType = ControlType.kPosition;
+
 
 }
