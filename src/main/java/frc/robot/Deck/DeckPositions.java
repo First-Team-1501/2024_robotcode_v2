55295014f351603f54963bdd11d253ebb83ff9bc
Lego56371 <ross.cuttriss@icloud.com>
2024-02-12 16:55:07 -0500

    changed numbers

--- src/main/java/frc/robot/Deck/DeckPositions.java
+++ src/main/java/frc/robot/Deck/DeckPositions.java
@@ -2,9 +2,9 @@ package frc.robot.Deck;
 
 public class DeckPositions {
 
-    public static double tolerance = 2;
+    public static double tolerance = 1;
     
-    public static double closeup = 20;
+    public static double closeup = 15;
     public static double climb = 0;
     public static double podium = 0;
     public static double backline = 0;
