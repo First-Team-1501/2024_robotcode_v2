c26d0aa4908e3faf05466f04b62b9af5f12a45b1
jtgarlits <jtgarlits@gmail.com>
2024-02-16 02:56:52 -0500

    last one

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem/ElevatorPositions.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem/ElevatorPositions.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems.ElevatorSubsystem;
 
 public class ElevatorPositions {
  
-    public static final double zero = 0;
+    public static final double zero = 1;
     public static final double intake = 43;
     
 }
