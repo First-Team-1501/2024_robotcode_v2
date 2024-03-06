2bf44602af4ad974b2c77529696815860883304f
Ross <ross.cuttriss@icloud.com>
2024-03-06 14:31:25 -0500

    Added the limelight stuff for setting Pipeline according to Alliance color

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -101,7 +101,7 @@ public class Robot extends TimedRobot {
   public void autonomousInit() {
     m_robotContainer.setMotorBrake(true);
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
+    m_robotContainer.getLimelight().setPipelineUsingAllianceColor();
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
@@ -117,6 +117,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
+    m_robotContainer.getLimelight().setPipelineUsingAllianceColor();
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
