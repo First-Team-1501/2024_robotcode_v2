63377624957d56cb63402760291ed738995ab4f9
Ross <ross.cuttriss@icloud.com>
2024-03-12 16:31:01 -0400

    Ready to Test

--- src/main/java/frc/robot/subsystems/leds/Leds.java
+++ src/main/java/frc/robot/subsystems/leds/Leds.java
@@ -4,15 +4,14 @@
 
 package frc.robot.subsystems.leds;
 
+import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.StrobeAnimation;
+import com.ctre.phoenix.led.TwinkleAnimation;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import java.util.Optional;
-import com.ctre.phoenix.led.CANdle;
-import com.ctre.phoenix.led.TwinkleAnimation;
-import com.ctre.phoenix.led.StrobeAnimation;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.limelight.LimelightHelpers;
+import java.util.Optional;
 
 public class Leds extends SubsystemBase {
 
@@ -20,6 +19,16 @@ public class Leds extends SubsystemBase {
   private TwinkleAnimation twinkleblueAnimation;
   private TwinkleAnimation twinkleredAnimation;
   private StrobeAnimation strobeAnimation;
+  private TwinkleAnimation twinkleblueguitarAnimation;
+  private TwinkleAnimation twinkleredguitarAnimation;
+  private TwinkleAnimation twinkleintakeAnimation;
+
+  private final int THUNDERSTUCK_LEDS = 68;
+  private final int GUITAR_LEDS = 80;
+  private final int TOTAL_LEDS = 148;
+  private final int GUITAR_LEDS_OFFSET = 68;
+  private final int THUNDERSTUCK_LEDS_OFFSET = 0;
+  private final double STROBE_SPEED = 98.0 / 256.0;
 
   private CANdle candle1;
 
@@ -29,13 +38,52 @@ public class Leds extends SubsystemBase {
   }
 
   private void setupLeds() {
-    twinkleblueAnimation = new TwinkleAnimation(0, 0, 255);
-    twinkleredAnimation = new TwinkleAnimation(255, 0, 0);
+    // twinkleblueAnimation = new TwinkleAnimation(0, 0, 255);
+    twinkleblueAnimation = new TwinkleAnimation(
+        0,
+        0,
+        255,
+        0,
+        0,
+        TOTAL_LEDS,
+        null,
+        THUNDERSTUCK_LEDS_OFFSET);
+    // twinkleredAnimation = new TwinkleAnimation(255, 0, 0);
+    twinkleredAnimation = new TwinkleAnimation(
+        255,
+        0,
+        0,
+        0,
+        0,
+        TOTAL_LEDS,
+        null,
+        THUNDERSTUCK_LEDS_OFFSET);
     candle1 = new CANdle(48, "canivore");
-    strobeAnimation = new StrobeAnimation(0, 255, 0, 0, 98.0 / 256.0, 68);
+    // strobeAnimation = new StrobeAnimation(0, 255, 0, 0, 98.0 / 256.0, 68);
+    strobeAnimation = new StrobeAnimation(0, 255, 0, 0, STROBE_SPEED, THUNDERSTUCK_LEDS, 0);
+    twinkleblueguitarAnimation = new TwinkleAnimation(
+        0,
+        0,
+        255,
+        0,
+        0,
+        GUITAR_LEDS,
+        null,
+        GUITAR_LEDS_OFFSET);
+    twinkleredguitarAnimation = new TwinkleAnimation(
+        255,
+        0,
+        0,
+        0,
+        0,
+        GUITAR_LEDS,
+        null,
+        GUITAR_LEDS_OFFSET);
+
+    twinkleintakeAnimation = new TwinkleAnimation(0, 0, 0, 255, STROBE_SPEED, TOTAL_LEDS, null, 0);
+    intaking = false;
 
     setLedsUsingAllianceColor();
-
   }
 
   @Override
@@ -44,18 +92,31 @@ public class Leds extends SubsystemBase {
         (LimelightHelpers.getTY("limelight") < 3) &&
         (LimelightHelpers.getTX("limelight") > -3) &&
         (LimelightHelpers.getTY("limelight") > -3) &&
-        LimelightHelpers.getTV("limelight"))  {
-
+        LimelightHelpers.getTV("limelight")) {
       setLedsToStrobe();
+    } else if (intaking) {
     } else {
       setLedsUsingAllianceColor();
     }
-
     // This method will be called once per scheduler run
   }
 
   public void setLedsToStrobe() {
     candle1.animate(strobeAnimation);
+    alliance = DriverStation.getAlliance();
+    if (alliance.isPresent()) {
+      if (alliance.get() == Alliance.Red) {
+        candle1.animate(twinkleredguitarAnimation);
+      }
+      if (alliance.get() == Alliance.Blue) {
+        // Put what you want it to do here.
+        candle1.animate(twinkleblueguitarAnimation);
+      }
+    }
+  }
+
+  public void setLedsIntake() {
+    // Put what you want them to do in here.
   }
 
   public void setLedsUsingAllianceColor() {
@@ -63,12 +124,16 @@ public class Leds extends SubsystemBase {
     if (alliance.isPresent()) {
       if (alliance.get() == Alliance.Red) {
         candle1.animate(twinkleredAnimation);
-
       }
       if (alliance.get() == Alliance.Blue) {
         // Put what you want it to do here.
         candle1.animate(twinkleblueAnimation);
       }
     }
+  }
+
+  public void setIntakeStatus(boolean status)
+  {
+    intaking = status;
   }
 }
