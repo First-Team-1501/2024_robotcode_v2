4e45f43b2bc0e0796684c92567cbbf84e3c6e0c4
Lego56371 <ross.cuttriss@icloud.com>
2024-04-11 09:58:34 -0400

    Hopefully added so when the forwarding is on the LED's strobe purple.

--- src/main/java/frc/robot/commands/shooter/ToggleForward.java
+++ src/main/java/frc/robot/commands/shooter/ToggleForward.java
@@ -5,16 +5,24 @@
 package frc.robot.commands.shooter;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.leds.Leds;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 
 public class ToggleForward extends Command {
   /** Creates a new ToggleForward. */
   public ToggleForward() {
     // Use addRequirements() here to declare subsystem dependencies.
+    // * This is for Shuffleboard
+    GenericEntry forwardingOn;
+    shuffleboardInit();
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    Leds.setForwardingStatus(false);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -28,5 +36,13 @@ public class ToggleForward extends Command {
   @Override
   public boolean isFinished() {
     return false;
+  }
+
+  public void shuffleboardInit() {
+    // * Put what you want on Shuffleboard here.
+  }
+
+  public void updateShuffleboard() {
+    // * Update Shuffleboard
   }
 }
